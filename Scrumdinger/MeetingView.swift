1228d2555311b9b5f885ec30145946719828a02b
naoki-kkc <naoki2kuchi@gmail.com>
2024-01-14 15:22:30 +0900

    using-stacks-to-arrange-views:section4

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -31,14 +31,20 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityLabel("10 minutes")
+            
             Circle()
                 .strokeBorder(lineWidth: 24)
+            
             HStack{
                 Text("Speaker 1 of 3")
                 Spacer()
                 Button(action: {}){
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel("Next speaker")
             }
         }
         .padding()
